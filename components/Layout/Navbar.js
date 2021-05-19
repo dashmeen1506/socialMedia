f8181c53aabec06fe7ec80b3ad4041357f6bf2f6
Dashmeen Singh <dashmeensingh@gmail.com>
2021-05-19 16:06:46 +0530

    database connected

---
+++ components/Layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Menu,Container,Icon} from 'semantic-ui-react'
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+
+
+function Navbar() {
+
+}
+
+export default Navbar;
